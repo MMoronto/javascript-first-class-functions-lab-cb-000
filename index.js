3255a3ef5df63682688c2200e83c836e3970307b
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-08 20:28:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function createMultiplier(multiplierValue) {
  }
 }
 
+function multiplier(a, b) {
+  return a*b
+}
+
 var doubler = createMultiplier(2);
 var tripler = createMultiplier(3);
 var doublerWithBind = multiplier.bind(null, 2);
